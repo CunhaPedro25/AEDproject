1333577a18150017453d5d05feecad0260bf0323
Pedro Cunha <pedro10cunha@gmail.com>
2022-11-23 23:23:43 +0000

    Updated struct to correct one

--- structures/equipamentos.h
+++ structures/equipamentos.h
@@ -1,7 +1,46 @@
+/*
+*  @brief Estrutura de dados para equipamentos
+*
+*/
+
 typedef struct Equipamentos{
-  int id;
-  char *nome;
-  char *data;
+
+  struct Aquisicao{
+    int dia;          // equipamento[0].data.dia
+    int mes;
+    int ano;
+  } data;
+
+  char *departamento;
+  int garantia;           // Em meses
+
+  struct CPUs{
+    char *name;
+    float clock;          // Velociade do relogio
+  } cpu;
+
+  struct Discos{
+    char *name;
+    int capacidade;       // Capacidade do disco
+  } disco;
+
+  char *ram;
+  char *sistema;
+
+  struct Aplicacoes{
+    char versao;
+    struct Validade{
+      int dia;
+      int mes;
+      int ano;
+    } validade;
+  } aplicaoes;
+
+  struct Placas{
+    char *ip;
+    char *mask;
+    char *broadcast;
+  } placas;
 }Equipamento;
 
 Equipamento equipamento[256];
