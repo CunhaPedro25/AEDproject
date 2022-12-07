6a18484bb152a2542582fa69936c30bdb3925758
Pedro Cunha <pedro10cunha@gmail.com>
2022-12-07 03:01:56 +0000

    Added table fucntions and files

---
+++ ui/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "ui.h"
+#include "utilities/colors.h"
+#include "../functions/functions.h"
+
+#endif //MAIN_H
---
+++ ui/table/table.h
@@ -0,0 +1,8 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "../main.h"
+
+void equipamentosTable();
+
+#endif //TABLE_H
