3847199fb14edaab8a408a6867667480a9811d3a
Pedro Cunha <pedro10cunha@gmail.com>
2022-12-07 03:04:48 +0000

    Created new folders and moved files around

--- ui/menu/menu.c
+++ ui/menu/menu.c
@@ -1,13 +1,5 @@
 #include "menu.h"
 
-void invalidOption(){
-  textColor(RED);
-  printf("[Opção Invalida]");    // Show error message
-  resetStyles();
-  restoreCursor();
-  clearToLineEnd();
-}
-
 /**
  *
  *  @brief Creates Menu with custom options and title
@@ -23,23 +15,30 @@ int menu(char *title, char **options, size_t size){
   int option = -1;
 
   clear();
-  titulo(title);
+  cursorVisibility(0);
+  renderTitle(title);
 
   for (int i = 0; i < size; ++i) {
-    printf("%d - %s\n", i+1, options[i]);
+    printf("    [%d] %s\n", i+1, options[i]);
   }
-  printf("0 - Sair");
+  printf("    [s] Sair");
 
   printf("\n\nOpção: ");
   do {
     saveCursor();
     char temporary[10];
 
+    cursorVisibility(1);
     fgets(temporary, 10, stdin);
+    cursorVisibility(0);
     fflush(stdin);
 
+    if((temporary[0] == 's' || temporary[0] == 'S') && temporary[1] == '\n'){
+      return 0;
+    }
+
     if(!isnumber(temporary)){
-      invalidOption();
+      showInvalidOption();
       continue;
     }
 
@@ -47,7 +46,7 @@ int menu(char *title, char **options, size_t size){
     option = strtol(temporary, &trash, 10);
 
     if(option < 0 || option > size) {
-      invalidOption();
+      showInvalidOption();
     }
   } while (option < 0 || option > size);
 
@@ -141,10 +140,10 @@ void equipamentosMenu(){
 
     switch (value) {
       case 1:
-      case 2:
-      case 3:
-      case 4:
         clear();
+        break;
+      case 2:
+        equipamentosTable();
         break;
 
       default:
