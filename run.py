6a3cc574a107555921d86f4b043dc3dcfc5c7b0c
Spectralpt <miguelfranquiralves@gmail.com>
2022-12-13 14:32:51 +0000

    added run.py support for windows

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os 
 from sys import path
+from sys import platform
 
 root = "."
 filesPy = []
@@ -7,17 +8,25 @@ fileType = '.c'
 filesDirs = ''
 seperator = ' '
 
+if platform == "linux" or platform == "linux2":
+    execute = "./app"
+    main = "./main.c"
+elif platform == "win32":
+    execute = ".\\app"
+    main = ".\main.c"
+
+
 for path, subdirs, files in os.walk(root):
     for name in files:
         relativeFilePath = os.path.join(path, name)
         if relativeFilePath[-2:] == '.c' and (path.find("cmake") == -1):
             filesPy.append(os.path.join(path, name))
 
-filesPy.remove("./main.c")
+filesPy.remove(main)
 
 for file in filesPy:
     filesDirs = seperator.join(filesPy)
 
-command = f"gcc main.c {filesDirs} -o app && ./app"
+command = f"gcc main.c {filesDirs} -o app && {execute}"
 print(command)
 os.system(command)
